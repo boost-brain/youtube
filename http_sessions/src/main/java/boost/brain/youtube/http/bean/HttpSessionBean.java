dcdb4f596079d94f22f990499321f7fdc96c502e
Sergey <sergiy.tychkin@gmail.com>
2020-01-16 11:47:57 +0200

    Update HttpSessionBean.java
    Need getter and setter.

--- http_sessions/src/main/java/boost/brain/youtube/http/bean/HttpSessionBean.java
+++ http_sessions/src/main/java/boost/brain/youtube/http/bean/HttpSessionBean.java
@@ -9,4 +9,12 @@ import org.springframework.web.context.annotation.SessionScope;
 @Component
 public class HttpSessionBean {
     private String name;
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
